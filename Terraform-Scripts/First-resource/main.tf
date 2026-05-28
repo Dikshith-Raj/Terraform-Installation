a07c99946530bd27021f2bf3995da57a22388943
Dikshith-Raj <dikshithta@gmail.com>
2026-05-28 13:55:47 +0530

    Update my first resource

---
+++ Terraform-Scripts/First-resource/main.tf
@@ -0,0 +1,12 @@
+provider "aws"{
+    region = "us-east-1"
+    access_key = ""
+    secret_key = ""
+
+}
+resource "aws_vpc" "myvpc"{
+    cide_block= "10.0.0.0/16"
+    tags = {
+        name = "Terraform-VPC"
+    }
+}
